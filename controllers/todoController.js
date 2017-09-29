07e4bfa253336cdb36ecfa9b5747a5e7f17653fd
Raul A. James <raul@nobi.io>
2017-09-29 10:16:08 +0200

    Full mongoDB support.

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -13,40 +13,40 @@ var todoSchema = new mongoose.Schema({
 // Todo : The collection
 var Todo = mongoose.model('Todo', todoSchema);
 
-var Item1 = Todo({item: 'buy flowers'}).save(function(err){
-  if (err) throw err;
-  console.log('item saved');
-});
-
-// dummy data
-var data = [{item: 'get milk'},{item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function (app) {
 
 
 app.get('/todo', function(req, res) {
-  res.render('todo', {todos:data});
+  // get data from mongodb and pass it to the view
+  // retrieve all the items on this connection
+  Todo.find({}, function(err, data) {
+    if (err) throw err;
+    res.render('todo', {todos:data});
+  });
 });
 
 // body parser will parse the post values in the body and
 // extend the request object.
 app.post('/todo', urlencodedParser, function(req, res) {
-
-  // add data to the array
-  data.push(req.body);
-  res.json(data); // return the new json object to the client/front-end.
-              // the ajax will reload the page (ejs) with the new json data.
+  // get data from view and add it to mongoDB
+  var newItem = Todo(req.body).save(function(err,data) {
+    if (err) throw err;
+    res.json(data); // return the new json object to the client/front-end.
+                    // the ajax will reload the page (ejs) with the new json data.
+  });
 });
 
 // :item acts as place holder for http query paramaters
 // stored in req.params.item
 app.delete('/todo/:item', function(req, res) {
-  // filter keeps the item if true otherwise it's removed
-  data = data.filter(function(todo) {
-    return todo.item.replace(/ /g, '-') !== req.params.item;
+  // delete the requested item from mongoDB
+
+  Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function (err,data) {
+    if (err) throw err;
+    res.json(data);
   });
-  res.json(data);
 });
 
 
