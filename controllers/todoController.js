73fbe39125a7b0900ad5ffff54f8753fa1544258
Raul A. James <raul@nobi.io>
2017-09-29 19:24:00 +0200

    Explicitly define a Promise for mongoose
    Removed the Deprecation Warning by mongoose of using the default Promise;
    mpromise

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,8 +1,16 @@
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+// use the native Promise
+// this eliminates the deprecationWarning by mongoose of using the default
+// promise; mpromise;
+mongoose.Promise = global.Promise;
+
 // connect to the database
-mongoose.connect('mongodb://test:test1234@ds151544.mlab.com:51544/mytodo', {useMongoClient: true});
+mongoose.connect('mongodb://test:test1234@ds151544.mlab.com:51544/mytodo', {useMongoClient: true}, function(err) {
+  if (err) throw err;
+  console.log('connected to database successfully!');
+});
 
 // create a schema - a blueprint
 var todoSchema = new mongoose.Schema({
